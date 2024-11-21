31b69050a9b3a28b348526efb7b033cc369e8698
Horatio <hs@horatio.cn>
2024-11-21 10:14:58 +0800

    refactor: reset repository

---
+++ README.md
@@ -0,0 +1,56 @@
+# Analysis Tool For HyperOS
+
+用于提取、修改和重命名 HyperOS 中的 APK 文件的 Python 脚本。它可以帮助开发者和用户轻松地获取 APK 文件及比对更新，并根据需要对其进行定制。
+
+## 功能
+
+- 从 ROM 下载链接下载 ROM
+- 从 ZIP 文件中提取 payload.bin
+- 从 payload.bin 文件中提取指定镜像文件
+- 提取镜像
+- 删除指定的 APK
+- 重命名 APK 文件
+- 更新 APK 版本
+- 更新 APK 文件名
+- 删除多余文件
+- 获取包信息
+
+## 如何使用
+
+1. 克隆此仓库：
+
+   ```
+   git clone https://github.com/HoratioShaw/Analysis-Tool-For-HyperOS.git
+   ```
+
+2. 确保已安装 Python 3.x, aria2c, 7zip 并安装依赖库：
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+3. 运行脚本：
+
+   ```
+   python main.py [-h] [-d URL] [-p] [-i] [-f] [-t] [-a] [-n] [-u] [-m] [-c] [-g] [-o]
+   ```
+
+按照提示选择相应的操作。
+
+```bash
+    -h, --help            显示此帮助消息并退出
+    -d URL, --download URL
+                          从指定 URL 下载 ROM
+    -p, --extract-payload
+                          从 zip 文件中提取 payload.bin
+    -i, --img             从 payload.bin 中提取指定镜像
+    -f, --files           从镜像中提取文件
+    -t  --devicetype      修改字典设备类型 (需要2个参数), 0/1 => 不备份/备份, ph/f/p => phone/fold/pad
+    -a, --apk             删除指定的 APK
+    -n, --rename          重命名 APK 文件
+    -u, --update-version  更新 APK 版本
+    -m, --update-name     更新 APK 名称
+    -c, --clean           删除不需要的文件和文件夹
+    -g, --git-push        上传数据库到仓库
+    -o, --get-info        获取包信息
+```
---
+++ config.py
@@ -0,0 +1,108 @@
+import os  # 引入 OS 模块，用于操作文件和目录
+import glob  # 引入 glob 模块，用于搜索文件夹中的文件
+import platform # 引入 platform 模块，用于读取设备信息
+
+# 获取当前脚本文件所在目录的绝对路径
+src_dir = os.path.abspath(__file__)
+
+# 在当前脚本文件所在目录下创建一个名为"output_apk"的子目录
+dst_dir = os.path.join(src_dir, "output_apk")
+
+# 在当前目录下搜索所有以".zip"为后缀的文件，并返回它们的文件路径
+zip_files = glob.glob("*.zip")
+
+# 创建名为"output_apk"的目录（如果它不存在）
+output_dir = "output_apk"
+
+update_apk_folder = "update_apk"
+
+update_apk_name_folder = "update_name_apk"
+
+if not os.path.exists(update_apk_folder):
+    os.makedirs(update_apk_folder)
+
+if not os.path.exists(update_apk_name_folder):
+    os.makedirs(update_apk_name_folder)
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# 获取名为"output_apk"目录中所有以".apk"为后缀的文件列表
+apk_files = [f for f in os.listdir(output_dir) if f.endswith(".apk")]
+
+# 外部工具路径
+tools_path_mapping = {
+                      ("Windows", "AMD64"): "./tools/Windows/AMD64/",
+                      ("Linux", "x86_64"): "./tools/Linux/x86_64/",
+                      ("Linux", "arm64"): "./tools/Linux/arm64/",
+                      ("Darwin", "x86_64"): "./tools/Darwin/x86_64/",
+                      ("Darwin", "arm64"): "./tools/Darwin/arm64/"
+                     }
+tools_path = tools_path_mapping.get((platform.system(), platform.machine()))
+
+# 定义了两个字符串常量，分别用于指定排除 APK 的文件路径和 APK 版本号和名称的 JSON 文件路径
+EXCLUDE_APK_PATH = "exclude_apk.txt"
+APK_VERSION = "app_version.json"
+APK_CODE = "app_code.json"
+APK_APP_NAME = "app_name.json"
+APK_APP_NAME_PAD = "app_name_pad.json"
+# 定义一个临时字典，用于存储版本名相同但版本号有所变更的 APK
+APK_CODE_NAME = "app_code_name.json"
+# 定义字典类型
+JSON_V = "app_json.txt"
+
+# 相关分区
+partitions = [
+              "product"
+             ]
+
+# 设备种类识别
+is_fold = [
+           "cetus", 
+           "zizhan", 
+           "babylon", 
+           "goku"
+          ]
+is_pad = [
+          "nabu", 
+          "elish", 
+          "enuma", 
+          "dagu", 
+          "pipa", 
+          "liuqin", 
+          "yudi", 
+          "yunluo", 
+          "xun", 
+          "sheng", 
+          "dizi", 
+          "ruan"
+         ]
+is_flip = [
+           "ruyi"
+          ]
+
+# 需要删除的文件夹
+files_to_delete = [
+                   "payload.bin", 
+                   "product.img", 
+                   "app_code_name.json"
+                  ]
+folders_to_delete = [
+                     "output_apk", 
+                     "update_apk", 
+                     "update_name_apk", 
+                     "product"
+                    ]
+
+# 获取信息
+properties = {
+              "ro.product.product.name": "设备名",
+              "ro.product.build.version.incremental": "软件版本号",
+              "ro.product.build.date": "编译时间",
+              "ro.product.build.id": "基线",
+              "ro.product.build.fingerprint": "指纹"
+             }
+
+prop_search_path = [
+                    "product"
+                   ]
